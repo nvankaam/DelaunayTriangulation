3f11b0f54d6baf4dc1f091f9c12cf1ff3e673aaf
Niels van Kaam <niels@vankaam.net>
2013-05-01 13:57:15 +0200

    Created simple random polygon method

---
+++ Algorithms/Util.cs
@@ -0,0 +1,28 @@
+﻿using GeoLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Util
+    {
+        /// <summary>
+        /// Creates a random convex polygon. Note the result is probably much smaller than the number of points given.
+        /// </summary>
+        /// <param name="generationSizeMin"></param>
+        /// <param name="generationSizeMax"></param>
+        /// <param name="boundarySize"></param>
+        /// <returns></returns>
+        public static C2DPolygon RandomConvexPolygon(int generationSizeMin, int generationSizeMax, double boundarySize = 1)
+        {
+            var boundRect = new C2DRect(-boundarySize, boundarySize, boundarySize, -boundarySize);
+            var randomPol = new C2DPolygon();
+            randomPol.CreateRandom(boundRect, generationSizeMin, generationSizeMax);
+            var hull = new C2DPolygon();
+            hull.CreateConvexHull(randomPol);
+            return hull;
+        }
+    }
+}
